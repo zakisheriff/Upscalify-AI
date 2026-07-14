22263dd3e4a13f3ea5b9590064a3c47f6e86e374
Zaki <zakisheriff57@gmail.com>
2026-07-14 15:59:45 +0530

    build: document ANIM_6 animation constant

--- lib/landing/constants/animations.ts
+++ lib/landing/constants/animations.ts
@@ -4,3 +4,4 @@ export const ANIM_2 = { duration: 300, delay: 200 };
 export const ANIM_3 = { duration: 400, delay: 000 };
 export const ANIM_4 = { duration: 500, delay: 100 };
 export const ANIM_5 = { duration: 100, delay: 200 };
+export const ANIM_6 = { duration: 200, delay: 000 };
