7d68cadeabd41e654ceec407b2d222be76e2a34d
Zaki <zakisheriff57@gmail.com>
2026-07-14 16:00:05 +0530

    feat: define config_28 configuration entry

--- lib/landing/config/entries.ts
+++ lib/landing/config/entries.ts
@@ -26,3 +26,4 @@ export const config_24 = { enabled: true, priority: 4 };
 export const config_25 = { enabled: true, priority: 5 };
 export const config_26 = { enabled: true, priority: 6 };
 export const config_27 = { enabled: true, priority: 7 };
+export const config_28 = { enabled: true, priority: 8 };
