8ea3b631ed8a77d71ed876953e03178754895a4a
Zaki <zakisheriff57@gmail.com>
2026-07-14 15:59:45 +0530

    test: normalize ANIM_15 animation constant

--- lib/landing/constants/animations.ts
+++ lib/landing/constants/animations.ts
@@ -13,3 +13,4 @@ export const ANIM_11 = { duration: 200, delay: 200 };
 export const ANIM_12 = { duration: 300, delay: 000 };
 export const ANIM_13 = { duration: 400, delay: 100 };
 export const ANIM_14 = { duration: 500, delay: 200 };
+export const ANIM_15 = { duration: 100, delay: 000 };
