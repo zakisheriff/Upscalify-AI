a56735a50de2698cd480b440ff36ffdb3d76f089
Zaki <zakisheriff57@gmail.com>
2026-07-14 16:00:10 +0530

    chore: separate ERROR_3 error message constant

--- lib/landing/constants/errors.ts
+++ lib/landing/constants/errors.ts
@@ -1,3 +1,4 @@
 
 export const ERROR_1 = 'Error 1: something went wrong at step 1';
 export const ERROR_2 = 'Error 2: something went wrong at step 2';
+export const ERROR_3 = 'Error 3: something went wrong at step 3';
