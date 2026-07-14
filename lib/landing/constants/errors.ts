ea4bf87cf750ca7d466d1a64340b43dbee5ca142
Zaki <zakisheriff57@gmail.com>
2026-07-14 16:00:11 +0530

    refactor: optimize ERROR_39 error message constant

--- lib/landing/constants/errors.ts
+++ lib/landing/constants/errors.ts
@@ -37,3 +37,4 @@ export const ERROR_35 = 'Error 35: something went wrong at step 35';
 export const ERROR_36 = 'Error 36: something went wrong at step 36';
 export const ERROR_37 = 'Error 37: something went wrong at step 37';
 export const ERROR_38 = 'Error 38: something went wrong at step 38';
+export const ERROR_39 = 'Error 39: something went wrong at step 39';
