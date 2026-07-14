8ee5b8ae0718b1255a2ba823c0d4e54d64c6577f
Zaki <zakisheriff57@gmail.com>
2026-07-14 15:59:47 +0530

    style: establish ANIM_81 animation constant

--- lib/landing/constants/animations.ts
+++ lib/landing/constants/animations.ts
@@ -79,3 +79,4 @@ export const ANIM_77 = { duration: 300, delay: 200 };
 export const ANIM_78 = { duration: 400, delay: 000 };
 export const ANIM_79 = { duration: 500, delay: 100 };
 export const ANIM_80 = { duration: 100, delay: 200 };
+export const ANIM_81 = { duration: 200, delay: 000 };
