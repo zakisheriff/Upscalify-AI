f30b4af3747052c4a52fe1981e8f6b24b295ac7a
Zaki <zakisheriff57@gmail.com>
2026-07-14 15:59:46 +0530

    docs: streamline ANIM_43 animation constant

--- lib/landing/constants/animations.ts
+++ lib/landing/constants/animations.ts
@@ -41,3 +41,4 @@ export const ANIM_39 = { duration: 500, delay: 000 };
 export const ANIM_40 = { duration: 100, delay: 100 };
 export const ANIM_41 = { duration: 200, delay: 200 };
 export const ANIM_42 = { duration: 300, delay: 000 };
+export const ANIM_43 = { duration: 400, delay: 100 };
