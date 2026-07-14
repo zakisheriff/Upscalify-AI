b983ad5cdcf32fc17f627aef1500810006aa5939
Zaki <zakisheriff57@gmail.com>
2026-07-14 15:59:47 +0530

    refactor: adjust ANIM_64 animation constant

--- lib/landing/constants/animations.ts
+++ lib/landing/constants/animations.ts
@@ -62,3 +62,4 @@ export const ANIM_60 = { duration: 100, delay: 000 };
 export const ANIM_61 = { duration: 200, delay: 100 };
 export const ANIM_62 = { duration: 300, delay: 200 };
 export const ANIM_63 = { duration: 400, delay: 000 };
+export const ANIM_64 = { duration: 500, delay: 100 };
