5a163bf4cbaa854a7e69a9f472f5220ba4e101b5
Zaki <zakisheriff57@gmail.com>
2026-07-14 15:59:45 +0530

    init: enable ANIM_8 animation constant

--- lib/landing/constants/animations.ts
+++ lib/landing/constants/animations.ts
@@ -6,3 +6,4 @@ export const ANIM_4 = { duration: 500, delay: 100 };
 export const ANIM_5 = { duration: 100, delay: 200 };
 export const ANIM_6 = { duration: 200, delay: 000 };
 export const ANIM_7 = { duration: 300, delay: 100 };
+export const ANIM_8 = { duration: 400, delay: 200 };
