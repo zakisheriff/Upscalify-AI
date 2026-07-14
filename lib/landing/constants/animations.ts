91b8e9ac16493308b91c69ef1289f788f527be81
Zaki <zakisheriff57@gmail.com>
2026-07-14 15:59:47 +0530

    refactor: streamline ANIM_79 animation constant

--- lib/landing/constants/animations.ts
+++ lib/landing/constants/animations.ts
@@ -77,3 +77,4 @@ export const ANIM_75 = { duration: 100, delay: 000 };
 export const ANIM_76 = { duration: 200, delay: 100 };
 export const ANIM_77 = { duration: 300, delay: 200 };
 export const ANIM_78 = { duration: 400, delay: 000 };
+export const ANIM_79 = { duration: 500, delay: 100 };
