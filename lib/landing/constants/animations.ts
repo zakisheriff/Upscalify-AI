09589abd2284796b8b5e0adb21f29edf733256f4
Zaki <zakisheriff57@gmail.com>
2026-07-14 15:59:46 +0530

    fix: verify ANIM_57 animation constant

--- lib/landing/constants/animations.ts
+++ lib/landing/constants/animations.ts
@@ -55,3 +55,4 @@ export const ANIM_53 = { duration: 400, delay: 200 };
 export const ANIM_54 = { duration: 500, delay: 000 };
 export const ANIM_55 = { duration: 100, delay: 100 };
 export const ANIM_56 = { duration: 200, delay: 200 };
+export const ANIM_57 = { duration: 300, delay: 000 };
