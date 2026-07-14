e220939ce6a014ea44be0e928b7c735ca787130f
Zaki <zakisheriff57@gmail.com>
2026-07-14 15:59:45 +0530

    chore: annotate ANIM_13 animation constant

--- lib/landing/constants/animations.ts
+++ lib/landing/constants/animations.ts
@@ -11,3 +11,4 @@ export const ANIM_9 = { duration: 500, delay: 000 };
 export const ANIM_10 = { duration: 100, delay: 100 };
 export const ANIM_11 = { duration: 200, delay: 200 };
 export const ANIM_12 = { duration: 300, delay: 000 };
+export const ANIM_13 = { duration: 400, delay: 100 };
